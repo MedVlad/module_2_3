badacfd7f26284d4599ebf91000447599b838457
Vladimir Medvedev <vmvlad84@gmail.com>
2024-07-06 08:34:02 +0300

    Break

--- module_2_4.py
+++ module_2_4.py
@@ -3,24 +3,23 @@ primes = []
 not_primes = []
 count_divider = 0
 for i in range(len(numbers)):
-    print(i)
+    if numbers[i] == 1:
+        continue
     is_prime = True
     count_divider = 0
     for j in range(numbers[i]):
         if numbers[i] % (j+1) == 0:
             count_divider += 1
-        if count_divider != 2:
-            is_prime = False
-        else:
-            is_prime = True
-    if numbers[i] == 1:
-        continue
+            if count_divider >= 3:
+                break
+    if count_divider != 2:
+        is_prime = False
     if is_prime:
         primes.append(numbers[i])
     else:
         not_primes.append(numbers[i])
-
 print("Primes: ", primes)
 print("Not Primes: ", not_primes)
+
 
 
