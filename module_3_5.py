04d19bf88afc3c15394cea901553caa90b8f4155
Vladimir Medvedev <vmvlad84@gmail.com>
2024-07-16 13:16:33 +0300

    module_3_5

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+
+def get_multiplied_digits(number):
+    first = 0
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        result = first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        result = first
+    return result
+
+
+result = get_multiplied_digits(40203)
+print(result)
