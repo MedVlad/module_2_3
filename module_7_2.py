e632e1d584f7ede584c17a6317ee261efdb400ef
Vladimir Medvedev <vmvlad84@gmail.com>
2024-08-24 21:55:41 +0300

    module_7_2

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i in range(0, len(strings)):
+        key = (i + 1, file.tell())
+        file.write(strings[i] + '\n')
+        strings_positions[key] = strings[i]
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
