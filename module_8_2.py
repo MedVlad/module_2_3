b134b368198fdd2f732fc39d9f4bd186ac24ecc1
Vladimir Medvedev <vmvlad84@gmail.com>
2024-08-29 10:39:59 +0300

    mopdule_8_2

--- module_8_2.py
+++ module_8_2.py
@@ -5,7 +5,6 @@ def personal_sum(numbers):
     for i in numbers:
         try:
             result += i
-
         except TypeError:
             print(f'Некорректный тип данных для подсчёта суммы -:{i}')
             incorrect_data += 1
@@ -22,14 +21,10 @@ def calculate_average(numbers):
         res = None
     except ZeroDivisionError:
         res = 0
-
     return res
 
 
-# print(personal_sum([1, "Строка", 3, "Ещё Строка"]))
-# print(personal_sum("1, 2, 3"))
-
 print(f'Результат 1: {calculate_average(("1, 2, 3"))}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
