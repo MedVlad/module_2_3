b9b4939eced8b2f948cc6d43f5e4cecfc78bcb04
Vladimir Medvedev <vmvlad84@gmail.com>
2024-09-05 14:08:51 +0300

    mopdule_9_2

--- module_9_2.py
+++ module_9_2.py
@@ -5,6 +5,7 @@ first_result = [len(x) for x in first_strings if len(x) >= 5]
 second_result = [(i, j) for i in first_strings for j in second_strings if len(i) == len(j)]
 third_result = {i: len(i)  for i in first_strings + second_strings if len(i) % 2 == 0 }
 
+
 print(first_result)
 print(second_result)
 print(third_result)
