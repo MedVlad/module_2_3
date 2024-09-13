798c4782026c2ab314def0b03cbcdaf6e2598a95
Vladimir Medvedev <vmvlad84@gmail.com>
2024-09-13 20:48:14 +0300

    module_9_7

---
+++ module_9_7.py
@@ -0,0 +1,27 @@
+def is_prime(func):
+    def wrapper(a, b, c):
+        prime = 0
+        num = func(a, b, c)
+        if num <= 1:
+            print('Составное')
+            return num
+        for i in range(1, num + 1):
+            if num % i == 0:
+                prime += 1
+
+        if prime == 2:
+            print('Простое')
+        else:
+            print('Составное')
+
+        return num
+
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+print(sum_three(2, 3, 6))
