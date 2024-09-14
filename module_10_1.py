a9d6fb129b9c4f3b3d836b7d7ea0233cf4139389
Vladimir Medvedev <vmvlad84@gmail.com>
2024-09-14 10:33:10 +0300

    module_10_1

---
+++ module_10_1.py
@@ -0,0 +1,40 @@
+from time import sleep
+from threading import Thread
+from datetime import datetime
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(word_count):
+            file.writelines(f"Привет! №{i + 1}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+
+time_start = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+time_stop = datetime.now()
+time_res = time_stop - time_start
+print(f"Работа функций:{time_res} секунд")
+
+time_start = datetime.now()
+thr_1 = Thread(target=write_words(word_count=10, file_name='example5.txt'))
+thr_2 = Thread(target=write_words(word_count=30, file_name='example6.txt'))
+thr_3 = Thread(target=write_words(word_count=200, file_name='example7.txt'))
+thr_4 = Thread(target=write_words(word_count=100, file_name='example8.txt'))
+
+thr_1.start()
+thr_2.start()
+thr_3.start()
+thr_4.start()
+
+thr_1.join()
+thr_2.join()
+thr_3.join()
+thr_4.join()
+time_stop = datetime.now()
+time_res = time_stop - time_start
+print(f"Работа потоков:{time_res} секунд")
