329cde464ceb74c023617d12e40356cf7e83b354
Vladimir Medvedev <vmvlad84@gmail.com>
2024-09-22 14:15:26 +0300

    module_9_6

--- module_9_6.py
+++ module_9_6.py
@@ -1,14 +1,21 @@
 def all_variants(text):
-    yield text[0]
-    yield text[1]
-    yield text[2]
-    yield text[0:2]
-    yield text[1:]
-    yield text[0:]
+    for i in range(len(text)):
+        yield text[i]
+    l = ''
+    for i in range(0, len(text) - 1):
+        l += text[i]
+    yield str(l)
+    l1 = ''
+    for i in range(1, len(text)):
+        l1 += text[i]
+    yield l1
+    l2 = ''
+    for i in range(len(text)):
+        l2 += text[i]
+    yield l2
 
 
-gen = all_variants('abc')
-print(gen)
+gen = all_variants('abcd')
 
 for i in gen:
     print(i)
