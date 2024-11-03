d9d469e1556607ae42f4b8c4e6a0d3e9552d6952
Vladimir Medvedev <vmvlad84@gmail.com>
2024-11-03 16:53:26 +0300

    TournamentTest

--- tests_12_2.py
+++ tests_12_2.py
@@ -26,21 +26,10 @@ class TournamentTest(TestCase):
         self.assertTrue(TournamentTest.all_results[MaxKey] == 'Ник')
 
     def test3(self):
-        tour = Tournament(90, self.runner1,self.runner2, self.runner3)
+        tour = Tournament(90, self.runner1, self.runner2, self.runner3)
         TournamentTest.all_results = tour.start().copy()
         MaxKey = max(TournamentTest.all_results)
         self.assertTrue(TournamentTest.all_results[MaxKey] == 'Ник')
-
-    def testLogic(self):
-        #for i in range(90):
-            TournamentTest.all_results = {}.copy()
-            self.runner1 = Runner('Усэйн', 10)
-            self.runner2 = Runner('Андрей', 9)
-            self.runner3 = Runner('Ник', 3)
-            tour = Tournament(7, self.runner1, self.runner2, self.runner3)
-            TournamentTest.all_results = tour.start().copy()
-            MaxKey = max(TournamentTest.all_results)
-            self.assertTrue(TournamentTest.all_results[MaxKey] == 'Ник', f"ошибка ")
 
 
 
