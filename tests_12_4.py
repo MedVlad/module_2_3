6e50e8e9b38069e5908ca78e6647c568051ccb3f
Vladimir Medvedev <vmvlad84@gmail.com>
2024-11-04 10:41:48 +0300

    logging

--- tests_12_4.py
+++ tests_12_4.py
@@ -35,7 +35,4 @@ class RunnerTest(TestCase):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, filemode='w', filename="runner_tests.log", encoding="UTF-8",
-                        format="%(asctime)s | %(levelname)s | %(message)s")
-
     unittest.main()
