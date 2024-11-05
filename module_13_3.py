31f948b7e44c881113e278989cdbd23c14700890
Vladimir Medvedev <vmvlad84@gmail.com>
2024-11-05 15:27:29 +0300

    module_13

---
+++ module_13_3.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api ="7368163380:AAFXPIDqjR64Vw62Fq4GqsbOSPZgR7o6v_0"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(text=['/start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def echo_message(message):
+     print(f'вы написали мне:{message.text}')
+     await message.answer(f'вы написали мне: {message.text}\n'
+                          f'Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
